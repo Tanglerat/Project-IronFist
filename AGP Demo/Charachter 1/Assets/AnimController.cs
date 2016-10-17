14003bf70b6691506023e790be0c8f1865d9ee20
Tanglerat <p0ppyb00ker>
2016-10-17 13:05:07 +0100

    Second Commit
    Updated Scripts for better behavious

---
+++ AGP Demo/Charachter 1/Assets/AnimController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour {
+	public string zpress;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("z")) {
+			GetComponent<Animator>().SetTrigger(zpress);
+	}
+}
+}
